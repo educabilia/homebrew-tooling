65643ac723c6906fd0cc94abb1a78ba160a58d06
Tommaso Patrizi <tommasop@thinkingco.de>
2017-05-03 11:30:10 +0200

    Update bs.rb

--- bs.rb
+++ bs.rb
@@ -3,7 +3,7 @@ require "formula"
 class Bs < Formula
   homepage "https://github.com/educabilia/bs#readme"
   url "https://github.com/educabilia/bs/archive/v0.2.1.tar.gz"
-  sha1 "4fbc7b28abc6b8da473fd122909a954e99ae1d88"
+  sha256 "4fbc7b28abc6b8da473fd122909a954e99ae1d88"
 
   def install
     bin.install "bin/bs" => "bs"
